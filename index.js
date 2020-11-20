868854b240edfae3c6d528ddcd5e24df69dbd869
Levi-Magny <Levi-Magny@users.noreply.github.com>
2020-11-20 01:27:04 -0300

    Aula-1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const http = require('http')
+const http = require('http');//carregando módulo 'http'
+
+let server = http.createServer((req, res)=>{
+    console.log('URL:', req.url);
+    console.log('METHOD:',req.method);
+
+    res.end('OK');
+});
+
+server.listen(3000, '127.0.0.1', ()=>{
+    console.log("o servidor está rodando!");
+});
